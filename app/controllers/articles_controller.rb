375bfba82fb0d3ba64025ca4c025b0a0c2bbf1b8
indra_abdullah <indra.abdullah99@gmail.com>
2015-02-04 16:29:10 +0700

    First Commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,58 @@
+class ArticlesController < ApplicationController
+  def index
+  	@article = Article.status_active
+  	#@article = Article.all
+  end
+
+  def new
+  	@article = Article.new
+  end
+
+  def create
+	@article = Article.new(params_article)
+    
+    if @article.save
+   		flash[:notice] = "Success Add Records"
+    	redirect_to action: 'index'
+    else
+    	flash[:error] = "Data Not Valid"
+    	render 'new'
+    end
+  end
+  
+  def show
+	@article = Article.find_by_id(params[:id])
+  end
+
+  def edit
+  	@article = Article.find_by_id(params[:id])
+  end
+
+  def update
+	@article = Article.find_by_id(params[:id])
+	if @article.update(params_article)
+	    flash[:notice] = "Success Update Records"
+	    redirect_to action: 'index'
+	else
+	    flash[:error] = "data not valid"
+	    render 'edit'
+	end
+   end
+
+   def destroy
+    @article = Article.find_by_id(params[:id])
+    if @article.destroy
+        flash[:notice] = "Success Delete a Records"
+        redirect_to action: 'index'
+    else
+        flash[:error] = "fails delete a records"
+        redirect_to action: 'index'
+    end
+   end
+   
+   private
+
+    def params_article
+        params.require(:article).permit(:title, :content, :status)
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrainingRuby::Application.config.secret_key_base = '430130bbffd7a86f6ce374b6f55fd802be41dcbc0c6742dda491e8fb8d0c3984fd0b669882cad9541659e9c7d11e9661cf97bf691d9ea302e95c7fe246411df8'
